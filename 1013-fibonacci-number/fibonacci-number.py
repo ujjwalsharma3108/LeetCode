134ea53c1ea6867577f94a68cb1ae5c0496616a6
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-08-11 16:49:37 +0530

    Time: 19 ms (56.08%) | Memory: 12.4 MB (16.47%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,17 @@
 class Solution(object):
-    def fib(self, n): 
-        if n >= 2:
-            return self.fib(n-1) + self.fib(n-2)
-        elif n == 1:
-            return 1
-        else :
-            return 0 
-        
+    def fib(self, n):
+        memo = {}
+
+        def solve(n):
+            if n <= 1:
+                return n
+
+            if n in memo:
+                return memo[n]
+
+            memo[n] = solve(n - 1) + solve(n - 2)
+            return memo[n]
+
+        return solve(n)
         
         
