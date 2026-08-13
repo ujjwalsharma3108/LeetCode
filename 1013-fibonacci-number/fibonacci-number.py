28a6a143592512ea5d2613330f31ceed03f5eda8
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-08-13 14:46:40 +0530

    Time: 14 ms (78.42%) | Memory: 12.5 MB (16.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def fib(self, n):
-        memo = {i: 0 for i in range(2, n + 1)}
+        memo = {}
 
         def series(n):
             if (n == 0 or n == 1):
                 return n
             
-            if memo[n] != 0 :
+            if n in memo :
                 return memo[n]
             
             else:
-                val1 = series(n-1)
-                val2 = series(n-2)
-                memo[n-1] = val1
-                memo[n-2] = val2
-                return val1 + val2
+                memo[n] = series(n - 1) + series(n - 2)
+                return memo[n]
         return series(n)
 
         
