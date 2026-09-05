d1ee729880e1e15303de63e100dfd5c7a6cc18cf
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-09-06 02:03:11 +0530

    Time: 115 ms (93.57%) | Memory: 33.3 MB (22.61%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -1,18 +1,20 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         min_arr = [nums[-1]]*len(nums)
-        i = 1
-        while i < len(nums):
-            if min_arr[len(nums)-i] > nums[len(nums)-i-1]:
-                min_arr[len(nums)-i -1] = nums[len(nums)-i-1]
+        i = len(nums)-2
+        min_val = nums[-1]
+        while i >= 0:
+            if min_val > nums[i]:
+                min_arr[i] = nums[i]
+                min_val = nums[i]
             else:
-                min_arr[len(nums)-i-1] = min_arr[len(nums)-i]
-            i+=1
+                min_arr[i] = min_val
+            i-=1
         max_val = nums[0]
         for i in range(len(nums)):
             if max_val < nums[i]:
                 max_val = nums[i]
-                
+
             if max_val - min_arr[i] <= k:
                 return i 
         return -1
