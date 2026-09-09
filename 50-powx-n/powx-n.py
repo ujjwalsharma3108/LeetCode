39c602f6c25d4914937e6b2a48c70427ff4762ba
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-09-09 14:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (20.75%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,18 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n < 0:
-            x = 1 / x
-            n = -n
-
+        if n < 0 :
+            x = 1/x
+            n = -1 * n
         
         def reoccurTask(x,n):
             if n == 0 :
                 return 1
-
+            
             half = reoccurTask(x,n//2)
-            if n % 2 == 1:
+
+            if n%2 ==1:
                 return x * half * half
             else:
                 return half * half
+            
 
         return reoccurTask(x,n)
