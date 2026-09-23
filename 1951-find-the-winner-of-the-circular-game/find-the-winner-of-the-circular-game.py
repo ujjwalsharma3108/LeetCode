87cdc64647e8be9aa703787b6356a1e9607273a8
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-09-24 01:26:01 +0530

    Time: 892 ms (5.02%) | Memory: 19.3 MB (61.29%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,6 +1,7 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         arr = [i for i in range(1,n+1)]
+        last = 0
         def reoccurTask(arr,present,count):
             if count == n -1:
                 return 
@@ -14,6 +15,7 @@ class Solution:
                 present+=1
             present = present % n 
             arr[present-1] = -1
+            last = present-1
             count+=1
             reoccurTask(arr,present,count)
         reoccurTask(arr,0,0)
