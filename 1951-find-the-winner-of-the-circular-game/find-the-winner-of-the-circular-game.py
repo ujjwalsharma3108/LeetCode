06292b3841de3582561da4c070a746673e0597c8
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-09-24 01:29:39 +0530

    Time: 1071 ms (5.02%) | Memory: 19.5 MB (5.43%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,28 +1,25 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        arr = [i for i in range(1,n+1)]
-        last = 0
-        def reoccurTask(arr,present,count):
-            if count == n -1:
-                return 
-            c = 0
-            while c != k :
-                present = present % n
-                if arr[present] == -1:
-                    present +=1
-                    continue
-                c+=1
-                present+=1
-            present = present % n 
-            arr[present-1] = -1
-            last = present-1
-            count+=1
-            reoccurTask(arr,present,count)
-        reoccurTask(arr,0,0)
+        arr = list(range(1, n + 1))
 
-        for i in arr:
-            if i != -1:
-                return i
+        def solve(present, remaining):
+            if remaining == 1:
+                return next(x for x in arr if x != -1)
+
+            count = 0
+
+            while count < k:
+                if arr[present] != -1:
+                    count += 1
+
+                if count < k:
+                    present = (present + 1) % n
+
+            arr[present] = -1
+
+            return solve((present + 1) % n, remaining - 1)
+
+        return solve(0, n)
         
 
         
