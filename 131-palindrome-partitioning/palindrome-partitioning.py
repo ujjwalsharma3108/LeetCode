6a0a47de0a9d6d997e40fd939ed7a954783a5de4
ujjwal sharma <153271453+ujjwalsharma3108@users.noreply.github.com>
2026-09-24 22:42:44 +0530

    Time: 47 ms (36.94%) | Memory: 33.4 MB (86.72%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,30 +2,29 @@ class Solution:
     def partition(self, s: str) -> list[list[str]]:
         ans = []
         partition = []
-
-        def isPalindrome(s):
-            if len(s) == 1:
-                return True
-
-            for i in range(len(s)//2):
-                if s[i] != s[len(s)-1-i]:
-                    return False
-            return True
-
-        def getAllParts(st,partition,ans):
-            if(len(st) == 0):
-                ans.append(partition.copy())
-                return
-            
-            for i in range(len(st)):
-                str_part = st[:i+1]
-
-                if isPalindrome(str_part):
-                    partition.append(str_part)
-                    getAllParts(st[i+1:],partition,ans)
-                    partition.pop()
-        getAllParts(s,partition,ans)
+        self.getAllParts(s,partition,ans)
         return ans
 
+    def getAllParts(self,st,partition,ans):
+        if(len(st) == 0):
+            ans.append(partition.copy())
+            return
+        
+        for i in range(len(st)):
+            str_part = st[:i+1]
+
+            if self.isPalindrome(str_part):
+                partition.append(str_part)
+                self.getAllParts(st[i+1:],partition,ans)
+                partition.pop()
+
+    def isPalindrome(self,s):
+        if len(s) == 1:
+            return True
+
+        for i in range(len(s)//2):
+            if s[i] != s[len(s)-1-i]:
+                return False
+        return True
         
 
